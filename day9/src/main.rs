a0c8976e2d4be2b4796952efbcef0b0a72f61455
Julian Priestley <jupriest@fb.com>
2020-12-12 19:21:55 +0000

    d9p2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -27,6 +27,29 @@ fn find_invalid(input: &[i64], window_size: usize) -> i64 {
     panic!("Didn't find invalid number!");
 }
 
+fn find_encryption_weakness(input: &[i64], target: i64) -> i64 {
+    let mut start = 0;
+    let mut end = 1;
+
+    loop {
+        let sum: i64 = input[start..end].iter().sum();
+
+        if sum == target {
+            let min: &i64 = input[start..end].iter().min().unwrap();
+            let max: &i64 = input[start..end].iter().max().unwrap();
+            return *min + *max;
+        }
+
+        if sum < target || start == end {
+            end += 1;
+        }
+
+        if sum > target {
+            start += 1;
+        }
+    }
+}
+
 fn main() {
     let file = File::open("input").unwrap();
     let reader = BufReader::new(file);
@@ -34,8 +57,9 @@ fn main() {
         .lines()
         .map(|l| l.unwrap().parse::<i64>().unwrap())
         .collect();
-    let result = find_invalid(&input, 25);
-    println!("Pt1: {}", result);
+    let invalid = find_invalid(&input, 25);
+    let weakness = find_encryption_weakness(&input, invalid);
+    println!("Pt1: {}, Pt2: {}", invalid, weakness);
 }
 
 #[cfg(test)]
@@ -50,5 +74,16 @@ mod tests {
         ];
         let result = find_invalid(&input, 5);
         assert_eq!(result, 127);
+    }
+
+    #[test]
+    fn pt2_example() {
+        let input: Vec<i64> = vec![
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ];
+        let invalid = find_invalid(&input, 5);
+        let result = find_encryption_weakness(&input, invalid);
+        assert_eq!(result, 62);
     }
 }
